2a63f01d314c3003d2e400789abc9c0dde9a7dfb
Kadir Özdinç <kozdinc92@gmail.com>
2023-06-07 16:09:49 +0300

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,53 @@
 
-/**
- * Example of OTAA device      
- * Authors: 
- *        Ivan Moreno
- *        Eduardo Contreras
- *  June 2019
- * 
- * This code is beerware; if you see me (or any other collaborator 
- * member) at the local, and you've found our code helpful, 
- * please buy us a round!
- * Distributed as-is; no warranty is given.
- */
-
 #include <Arduino.h>
 #include <lorawan.h>
-
 
 // OTAA credentials
 const char *devEui = "80B3D57ED004397D";
 const char *appEui = "1300000000000013";
 const char *appKey = "47521E11573093C237C7333983DD475C";
 
-const unsigned long interval = 15000;    // 10 s interval to send message
-unsigned long previousMillis = 0;  // will store last time message sent
-unsigned int counter = 0;     // message counter
+const unsigned long interval = 15000; // 10 s interval to send message
+unsigned long previousMillis = 0;     // will store last time message sent
+unsigned int counter = 0;             // message counter
 
 char myStr[50];
 char outStr[255];
 byte recvStatus = 0;
 
+#define LED 1
+
 const sRFM_pins RFM_pins = {
-  .CS = 7,
-  .RST = 3,
-  .DIO0 = 18,
-  .DIO1 = 19,
-  // .DIO2 = -1,
-  // .DIO5 = -1,
+    .CS = 7,
+    .RST = 3,
+    .DIO0 = 18,
+    .DIO1 = 19,
+    // .DIO2 = -1,
+    // .DIO5 = -1,
 };
 
-void setup() {
+void setup()
+{
   // Setup loraid access
   Serial.begin(115200);
-  while(!Serial);
-  if(!lora.init()){
+  while (!Serial)
+    ;
+  if (!lora.init())
+  {
     Serial.println("RFM95 not detected");
     delay(5000);
     return;
   }
-
+  pinMode(LED, OUTPUT);
   // Set LoRaWAN Class change CLASS_A or CLASS_C
   lora.setDeviceClass(CLASS_C);
 
   // Set Data Rate
-  lora.setDataRate(SF10BW125);
+  lora.setDataRate(SF8BW125);
 
   // set channel to random
   lora.setChannel(MULTI);
-  
+
   // Put OTAA Key and DevAddress here
   lora.setDevEUI(devEui);
   lora.setAppEUI(appEui);
@@ -64,35 +55,52 @@ void setup() {
 
   // Join procedure
   bool isJoined;
-  do {
+  do
+  {
     Serial.println("Joining...");
     isJoined = lora.join();
-    
-    //wait for 10s to try again
+
+    // wait for 10s to try again
     delay(10000);
-  }while(!isJoined);
+  } while (!isJoined);
   Serial.println("Joined to network");
+
+  sprintf(myStr, "First-%d", counter);
+
+  Serial.print("Sending: ");
+  Serial.println(myStr);
+  lora.sendUplink(myStr, strlen(myStr), 0, 1);
 }
 
-void loop() {
+void loop()
+{
   // Check interval overflow
-  if(millis() - previousMillis > interval) {
-    previousMillis = millis(); 
+  if (millis() - previousMillis > interval)
+  {
+    previousMillis = millis();
 
-    sprintf(myStr, "Counter-%d", counter); 
+    sprintf(myStr, "Counter-%d", counter);
 
     Serial.print("Sending: ");
     Serial.println(myStr);
-    
+    //data,length,confirmed?,port
     lora.sendUplink(myStr, strlen(myStr), 0, 1);
     counter++;
+    // Serial.println(lora.readAck());
   }
 
   recvStatus = lora.readData(outStr);
-  if(recvStatus) {
+  if (recvStatus)
+  {
+    Serial.println(lora.getRssi());
     Serial.println(outStr);
+    if (outStr[0] == 0x41) // A gelmişse led on
+      digitalWrite(LED, HIGH);
+    else
+      digitalWrite(LED, LOW); // farklı veri ise off
   }
-  
+
   // Check Lora RX
   lora.update();
+
 }
